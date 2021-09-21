173c88028222a5dca69551169eb060614cadfe23
hyunwoo <kiesland1121@naver.com>
2021-09-21 19:08:31 +0900

    create bean and path variable

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ https://www.inflearn.com/course/spring-boot-restful-web-services#curriculum
 
 수업에서는 Maven, Postman으로 API 통신하는 실습환경이었지만,
 해당 프로젝트에서는 Gradle, Swagger 로 실습 환경을 갖춰져있습니다.
+
+2021 0921
+
+~ chapter.2
--- src/main/java/com/inflearn/lecture/controller/HelloWorldController.java
+++ src/main/java/com/inflearn/lecture/controller/HelloWorldController.java
@@ -1,17 +1,28 @@
 package com.inflearn.lecture.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/v1")
 public class HelloWorldController {
-    //GET
-    // /hello-world (endpoint)
-    // RequestMapping(Method=RequestMethod.GET, path="/hello-world" <<예전방식
-    @GetMapping(path = "/hello-world")
-    public String helloWorld(){
-        return "hello-world";
+  // GET
+  // /hello-world (endpoint)
+  // RequestMapping(Method=RequestMethod.GET, path="/hello-world" <<예전방식
+  @GetMapping(path = "/hello-world")
+  public String helloWorld() {
+    return "hello-world";
+  }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("hello-world");
     }
+
+  @GetMapping(path = "/hello-world-bean/path/{id}")
+  public HelloWorldBean helloWorldBeanPath(@PathVariable String id) {
+    return new HelloWorldBean(String.format("hello-world, %s",id));
+  }
 }
